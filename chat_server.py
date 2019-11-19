3c63d8b02a111ba021bcff0cae6f4be7fe662dbf
Tedu <18811408518@163.com>
2019-11-19 21:39:11 +0800

    update server

--- chat_server.py
+++ chat_server.py
@@ -15,6 +15,7 @@ class ChatServer:
         self.__rlist = [self.__sockfd]  # 监控读事件IO，此时里面放入了监听套接字
         self.__wlist = []  # 监控写事件IO
         self.__xlist = []  # 监控错误事件IO
+        self.
 
     def __create_socket(self):
         """
@@ -25,13 +26,45 @@ class ChatServer:
         sockfd.bind(self.__addr)
         return sockfd
 
+    def __handle(self, connfd):
+        request = connfd.recv(1024)
+        if not request:
+            return "Q"
+        if request.decode().spilt(" ") == "U":
+            self.__verify_user()
+        elif request.decode().spilt(" ") == "P":
+            pass
+        elif request.decode().spilt(" ") == "C":
+            pass
+        elif request.decode().spilt(" ") == "F":
+            pass
+        elif request.decode().spilt(" ") == "L":
+            pass
+        elif request.decode().spilt(" ") == "G":
+            pass
+        elif request.decode().spilt(" ") == "D":
+            pass
+        else:
+            pass
+
+    def __verify_user(self):
+        pass
+
     def __select_concurent(self):
+        """
+            IO多路复用方式，循环监控各IO事件
+        """
         while True:
             rs, ws, xs = select(self.__rlist, self.__wlist, self.__xlist)
             for item in rs:
                 if item is self.__sockfd:
                     connfd, addr = self.__sockfd.accept()
                     self.__rlist.append(connfd)
+                else:
+                    if self.__handle(item) == "Q":
+                        self.__rlist.remove(item)
+                        item.close()
+                        continue
 
     def serve_forever(self):
         self.__sockfd.listen(5)  # 设置为监听套接字
